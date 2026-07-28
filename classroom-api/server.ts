c871a277d1ef6b8917960884d8c7e483419c794e
Arniode <toluwanimiadediran2@gmail.com>
2026-07-28 16:41:00 +0100

    classroom API with JWT authentication middleware, get single student and update

--- classroom-api/server.ts
+++ classroom-api/server.ts
@@ -1,4 +1,4 @@
-import express, {type Express,type Request, type Response } from 'express';
+import express, {type Express,type Request, type Response, type NextFunction } from 'express';
 import { Classroom } from './classroom';
 import { Student } from './student'
 import jwt from 'jsonwebtoken';
@@ -49,9 +49,51 @@ app.post('/login', (req: Request, res:Response ) => {
   res.status(200).json({message:"Successfuly Logged in.", token:token})
 })
 
+function authenticateToken(req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization;
 
+  if (!authHeader) {
+    return res.status(401).json({ error: "Unauthorized: No token provided" });
+  }
+  
+  const token = authHeader.split(' ')[1];
+
+  try {
+    jwt.verify(token, JWT_SECRET);
+    next();
+  } catch (error) {
+    return res.status(403).json({ error: "Invalid token" });
+  }
+}
+
+app.get('/students/:id', authenticateToken,(req:Request, res:Response)=>{
+  const {id}= req.params;
+  const students = myClassroom.getStudents();
+  const student = students.find((student)=> student.id === Number(id));
+
+    if(!students) {
+      res.status(400).json({error:`Student doesn't exist!`})
+      return;
+    }
+    else{
+      res.status(200).json(student)
+    }
+})
+
+app.patch('/students/:id',authenticateToken,(req:Request, res:Response)=> {
+  const {id} = req.params;
+  const {name} = req.body
+  const students = myClassroom.getStudents()
+  const student = students.find((student)=> student.id === Number(id))
+
+  if(!student) { 
+    res.status(404).json({error:`NOT FOUND`})
+  }
+  else{
+    res.status(200).json(student)
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
