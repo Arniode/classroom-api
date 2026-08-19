5828385d1db93146ca1a5653735de559341e4e97
Arniode <toluwanimiadediran2@gmail.com>
2026-08-19 09:37:24 +0100

    Beautified code

--- classroom-api/Mongoose-practice.ts
+++ classroom-api/Mongoose-practice.ts
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
 const gradeSchema = new mongoose.Schema({
-    subject: {type: String, required:true},
-    value: {type: Number, required:true}
+    subject: {type: String, required: true},
+    value: {type: Number, required: true}
 })
 
 const studentSchema = new mongoose.Schema({
     name: {type: String, required: true},
     password: {type: String, required: true},
-    grades:[gradeSchema]
+    grades: [gradeSchema]
 },
 {versionKey: false
 })
